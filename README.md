4cd45fd3b51b938fce9a56fe582be4c2a7dc85ea
Albin Bjork <devalbinbjork@gmail.com>
2023-09-29 12:35:47 +0200

    v1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # workhours-calculator
-Simple script to calculate worked hours in a day, because I'm too lazy to do simple math.
+Simple python script to calculate worked hours in a day, because I'm too lazy to do simple math at the end of a workday.
+
+## Example
+![example](./img/example.png)
+
+## How it works
+*Requires python and datetime module.*
+
+Simply run:
+```
+python3 ./calc.py
+```
+Then answer prompted questions.
+
+## If changes are necessary
+You can change default workday and default lunchtime in the script on line 3 & 4.
+```
+default_workday = datetime.timedelta(hours=8)
+default_lunch_time = datetime.timedelta(hours=1)
+```
+These are set by default to 8 hour workday with 1 hour lunch.
---
+++ calc.py
@@ -0,0 +1,33 @@
+import datetime
+
+default_workday = datetime.timedelta(hours=8)
+default_lunch_time = datetime.timedelta(hours=1)
+
+start_time_str = input("Enter the start time (HH:MM): ")
+start_time = datetime.datetime.strptime(start_time_str, "%H:%M")
+
+end_time_str = input("Enter the end time (HH:MM): ")
+end_time = datetime.datetime.strptime(end_time_str, "%H:%M")
+
+time_elapsed = end_time - start_time
+
+lunch_time_minutes = int(input("Enter the length of your lunch break (in minutes): "))
+remaining_lunch = default_lunch_time - datetime.timedelta(minutes=lunch_time_minutes)
+
+total_worktime = (time_elapsed - default_lunch_time) + remaining_lunch
+flex_time = total_worktime - default_workday
+
+total_worktime_str = str(total_worktime).split(":")[:2]
+formatted_total_worktime = ":".join(total_worktime_str)
+
+if flex_time < datetime.timedelta(0):
+    flex_time_str = str(-flex_time).split(":")[:2]
+    formatted_flex_time = "-" + ":".join(flex_time_str)
+else:
+    flex_time_str = str(flex_time).split(":")[:2]
+    formatted_flex_time = ":".join(flex_time_str)
+
+print("-" * 50)
+print(f"Total Time Worked: {formatted_total_worktime}")
+print(f"Flex Time: {formatted_flex_time}")
+print("-" * 50)
